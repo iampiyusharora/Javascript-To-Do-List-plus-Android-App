f9a6af6a379360cdd3026f6e5275cfcd0a489a82
Piyush Arora <piyu.live@live.com>
2017-02-22 16:28:59 -0600

    A Simple JS based To Do List app with Android Source Code

---
+++ android-app/app/src/androidTest/java/co/arora/piyush/vanillatodo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package co.arora.piyush.vanillatodo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
